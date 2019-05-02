888fb4a7c18b4797ba18b153306129f8454ec1df
Phyllis Kan <pyk9q7@gmail.com>
2019-05-02 21:36:44 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,22 @@
 def division(num1, num2)
- 
+  num1/num2
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
 end
 
 def argue
-  
+  phrase = "I'm right and you are wrong!"
 end
 
 def greeting
-
+  greeting = "Hi there,"
+  name = "Bobby"
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
